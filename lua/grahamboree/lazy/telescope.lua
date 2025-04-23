71ffb0de6336fd695c9b5cdb631d6f09cf111687
Graham Pentheny <graham.pentheny@gmail.com>
2025-04-23 14:48:35 -0400

    tweaked telescope keybinds

--- lua/grahamboree/lazy/telescope.lua
+++ lua/grahamboree/lazy/telescope.lua
@@ -4,7 +4,8 @@ return {
     tag = "0.1.5",
 
     dependencies = {
-        "nvim-lua/plenary.nvim"
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons"
     },
 
     config = function()
@@ -12,7 +13,8 @@ return {
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
