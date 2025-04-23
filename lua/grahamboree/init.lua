2505dd54595e02aee3a713189db8658942a822c8
graham.pentheny@gmail.com <graham.pentheny@gmail.com>
2025-04-23 13:10:03 -0400

    cleanup init.lua

--- lua/grahamboree/init.lua
+++ lua/grahamboree/init.lua
@@ -2,15 +2,10 @@ require("grahamboree.set")
 require("grahamboree.remap")
 require("grahamboree.lazy_init")
 
-local augroup = vim.api.nvim_create_augroup
-local grahamboreeGroup = augroup('grahamboree', {})
-
 local autocmd = vim.api.nvim_create_autocmd
-local yank_group = augroup('HighlightYank', {})
 
 -- highlight yanked text briefly
 autocmd('TextYankPost', {
-    group = yank_group,
     pattern = '*',
     callback = function()
         vim.highlight.on_yank({
@@ -22,34 +17,28 @@ autocmd('TextYankPost', {
 
 -- trim trailing whitespace on save
 autocmd({"BufWritePre"}, {
-    group = grahamboreeGroup,
     pattern = "*",
     command = [[let save_cursor = getpos(".") | %s/\s\+$//e | call setpos('.', save_cursor)]],
 })
 
--- for .bytes files in Unity
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
-  pattern = "*.bytes",
-  command = "set filetype=yaml"
-})
-
+-- LSP keybinds
 autocmd('LspAttach', {
-    group = grahamboreeGroup,
     callback = function(e)
         local opts = { buffer = e.buf }
-        vim.keymap.set("n", "<F12>", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        --vim.keymap.set("n", "<F12>", function() vim.lsp.buf.definition() end, opts)
+        --vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        --vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end, opts)
+        --vim.keymap.set("n", "<leader>lws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        --vim.keymap.set("n", "<leader>lca", function() vim.lsp.buf.code_action() end, opts)
+        --vim.keymap.set("n", "<leader>lrr", function() vim.lsp.buf.references() end, opts)
+        --vim.keymap.set("n", "<leader>lrn", function() vim.lsp.buf.rename() end, opts)
+        --vim.keymap.set("i", "<leader>lh", function() vim.lsp.buf.signature_help() end, opts)
         --vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
         --vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
     end
 })
 
+-- file type aliases
 vim.filetype.add({
     extension = {
         jenkins = "groovy",
@@ -57,6 +46,7 @@ vim.filetype.add({
     },
 })
 
+-- Diagnostics
 vim.diagnostic.config({
   virtual_text = true,   -- Show inline text
   signs = true,          -- Show signs in the gutter
