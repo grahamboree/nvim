5cfc456b98c8bf2f1a0a70dbaec42675d277a0c7
Graham Pentheny <graham.pentheny@gmail.com>
2025-04-25 22:56:56 -0400

    Update telescope remaps
    Added g to live-grep the word under the cursor, or the current visual selection

--- lua/grahamboree/lazy/telescope.lua
+++ lua/grahamboree/lazy/telescope.lua
@@ -1,32 +1,20 @@
 return {
-    "nvim-telescope/telescope.nvim",
-
-    tag = "0.1.5",
-
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
     dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons"
+        'nvim-lua/plenary.nvim',
+        'nvim-tree/nvim-web-devicons'
     },
-
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup{}
+        local telescope = require('telescope.builtin')
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-        vim.keymap.set('n', '<leader>pws', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>pWs', function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        local utils = require('grahamboree.utils')
+        utils.nremap('<leader>pf', telescope.find_files)
+        utils.nremap('<leader>pg', telescope.live_grep)
+        utils.nremap('<leader>vh', telescope.help_tags)
+        utils.nremap('<leader>g', function() telescope.live_grep { default_text = vim.fn.expand('<cword>') } end)
+        utils.vremap('<leader>g', function() telescope.live_grep { default_text = utils.getVisualText() } end)
+        --utils.nremap('<leader>pWs', function() telescope.grep_string { search = vim.fn.expand('<cWORD>') } end)
     end
 }
-
