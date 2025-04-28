419f32fb69b7b61b679352c9c785410ddf3ea405
graham.pentheny@gmail.com <graham.pentheny@gmail.com>
2025-04-28 13:22:14 -0400

    Added folder ignore patterns for telescope

--- lua/grahamboree/lazy/telescope.lua
+++ lua/grahamboree/lazy/telescope.lua
@@ -6,15 +6,28 @@ return {
         'nvim-tree/nvim-web-devicons'
     },
     config = function()
-        require('telescope').setup{}
-        local telescope = require('telescope.builtin')
+        local builtin = require('telescope.builtin')
+        require('telescope').setup({
+            defaults = {
+                file_ignore_patterns = {
+                    "node_modules",
+                    "%.git/",
+                    "Engine/", -- for Spirit Crossing
+                    "venv/",
+                    ".venv/",
+                    "build/",
+                    "dist/",
+                    "obj/",
+                    "__pycache__/",
+                }
+            }
+        })
 
         local utils = require('grahamboree.utils')
-        utils.nremap('<leader>pf', telescope.find_files)
-        utils.nremap('<leader>pg', telescope.live_grep)
-        utils.nremap('<leader>vh', telescope.help_tags)
-        utils.nremap('<leader>g', function() telescope.live_grep { default_text = vim.fn.expand('<cword>') } end)
-        utils.vremap('<leader>g', function() telescope.live_grep { default_text = utils.getVisualText() } end)
-        --utils.nremap('<leader>pWs', function() telescope.grep_string { search = vim.fn.expand('<cWORD>') } end)
+        utils.nremap('<leader>pf', builtin.find_files)
+        utils.nremap('<leader>pg', builtin.live_grep)
+        utils.nremap('<leader>vh', builtin.help_tags)
+        utils.nremap('<leader>g', function() builtin.live_grep { default_text = vim.fn.expand('<cword>') } end)
+        utils.vremap('<leader>g', function() builtin.live_grep { default_text = utils.getVisualText() } end)
     end
 }
