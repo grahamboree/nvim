aaa53fc2e6d66fa6737bba4b56136279c34367f4
Graham Pentheny <graham.pentheny@gmail.com>
2026-03-08 13:21:50 -0400

    Fix lazy, telescope and treesitter version incompatibilities

--- lua/grahamboree/lazy/telescope.lua
+++ lua/grahamboree/lazy/telescope.lua
@@ -1,9 +1,8 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
+    'nvim-telescope/telescope.nvim', version = '*',
     dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons"
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
     config = function()
         require("telescope").setup {
